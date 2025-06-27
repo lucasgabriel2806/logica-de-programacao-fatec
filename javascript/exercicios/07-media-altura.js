8af0c004e1f454a33d49ff80acf012ed80f2f713
Lucas Gabriel de Paula Pinto <lucas.gabriellgpp@gmail.com>
2025-06-27 08:19:54 -0300

    Exercicio media-altura

---
+++ javascript/exercicios/07-media-altura.js
@@ -0,0 +1,20 @@
+// readline-sync
+
+const read = require('readline-sync');
+
+let i, altura, media, total;
+
+total = 0;
+
+// Inicio Programa
+for (i = 1; i <= 6; i++) {
+
+  altura = parseFloat(read.question(`DIGITE A ALTURA DO ${i}o JOGADOR: `));
+  
+  total = total + altura;
+
+}
+
+media = total / 6
+
+console.log(`MEDIA DA ALTURA DOS JOGADORES: ${media.toFixed(2)}`);
